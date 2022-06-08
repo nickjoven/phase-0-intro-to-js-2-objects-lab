124e054ebe685222e353c06240f4889528c43c04
Nicholas <nicholasjoven@gmail.com>
2022-06-07 23:45:33 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+// Program purpose: keep record of employees
+
+// First initialize employee object
+const employee = {
+    name: "Muffin Man",
+    streetAddress: "Drury Lane" 
+};
+
+// Next create function as decribed in readme
+// Personal note, obj,[key]: value is standard, colon signifies key val pair
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj,
+        [key]: value,
+    };
+};
+
+// Next function destructive update
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+// Delete employee by key nondestructively (spread)
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj }
+    delete newObj[key];
+    return newObj;
+};
+
+// Delete employee by key destructively
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
+
+// I guess?
